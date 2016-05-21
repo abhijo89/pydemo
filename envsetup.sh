be147027fbf91e386588890ab98adf50698a6dea
Abhilash Joseph C <abhilash@mangaloreinfotech.in>
2016-05-21 14:06:11 +0530

    First commit for config setup

--- envsetup.sh
+++ envsetup.sh
@@ -5,5 +5,9 @@ sudo apt-get install python-pip python-dev nginx -y
 # PIP has not -y option :(
 yes w | sudo pip install gunicorn flask
 
-sudo ln -s /home/codaxtr_user/pydemo/appserver.conf /etc/init/appserver.conf
+sudo cp /home/codaxtr_user/pydemo/appserver.conf /etc/init/appserver.conf
 sudo start appserver
+
+sudo rm -rf /etc/nginx/sites-available/default
+
+sudo cp /home/codaxtr_user/pydemo/nginx_appserver.conf /etc/nginx/sites-available/appserver
